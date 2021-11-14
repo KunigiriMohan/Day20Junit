120aa023a7014d81765ff4f22840c68d94080d6d
KunigiMohan <kmohanr5@gmail.com>
2021-11-14 18:02:41 +0530

    Added : Checking Password added containing atleast one Upper case.

--- src/PasswordVerify.java
+++ src/PasswordVerify.java
@@ -1,20 +1,19 @@
-package src;
-
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class PasswordVerify {
-
     /*
      * passwordVerify() method to verify password entered by user is same according to regex
      * */
     public boolean passwordVerify(String password){
-        if (password==null&& password.isEmpty()){
-            return false;
+        if(password==null&&password.isEmpty())      //Checking input Password is empty or not
+        {
+            return true;
         }
-        String passwordRegex ="^[a-z,A-Z]{8,}";         //Regex for checking Password
-        Pattern pattern= Pattern.compile(passwordRegex);
-        if(pattern.matcher(password).matches()){        //Checking input password matching regex or not
+        String regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*d)[a-zA-Zd]{8,}$";    //Regex code for verifying Password
+        Pattern pattern= Pattern.compile(regex);
+        if (pattern.matcher(password).matches())        //Checking entered password matches with regex code or not.
+        {
             return true;
         }
         else
