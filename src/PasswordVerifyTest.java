720b459bf4cbe4ab54febd518028ae0a350d36ed
KunigiMohan <kmohanr5@gmail.com>
2021-11-14 18:05:03 +0530

    Added : Testing passwordVerify method in PasswordVerify class.

--- src/PasswordVerifyTest.java
+++ src/PasswordVerifyTest.java
@@ -1,14 +1,13 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import src.PasswordVerifyUC5;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordVerifyTest {
 
-    @ParameterizedTest                  //Declaring Test as parameterized Test
-    @ValueSource(strings={"abcdefghi","abc","abcdefgh","Abdfddfdf"})            //declaring values through valueSource
+    @ParameterizedTest
+    @ValueSource(strings = {"avfasdAa","Afdfdfds","fadfsdfsdsd","AAAAAAAADFDDFF"})
     void passwordVerify(String password) {
         PasswordVerify obj =new PasswordVerify();             //Creating object of PasswordVerifyUC5 class
         assertEquals(true,obj.passwordVerify(password));
